6a1ceff9162a7e8b6f514fe2d708f8536e0a9764
Jucrz <juliencruz78@gmail.com>
2021-05-31 23:32:19 +0200

    Fonction connection

--- BDD.py
+++ BDD.py
@@ -8,5 +8,31 @@ Create_BDD.Create_table()
 connection = pymysql.connect(host="localhost",user="root",passwd="",database="b2_proj_dev_log" )
 cursor = connection.cursor()
 
+ConnectionUser = False
+
+a = 0
+while not ConnectionUser:
+    print("Nom de compte :")
+    ndc = input()
+    cursor.execute("Select * from Joueur where Name = %s", ndc)
+    test = "0"
+    for row in cursor:
+        test = row
+    print(test)
+    print("Mot de passe :")
+    mdp = input()
+    cursor.execute("Select * from Joueur where Password = %s", mdp)
+    test2 = "1"
+    for row in cursor:
+        test2 = row
+    print(test2)
+    if test == test2:
+        ConnectionUser = True
+    else:
+        print("Nom de compte ou mot de passe incorrecte.")
+
+if ConnectionUser:
+    print("Tu es connecté !")
+
 # database close
 connection.close()
