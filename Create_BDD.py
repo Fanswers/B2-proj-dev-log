6099d2f7eff15349592476e307444b1c4995b09a
Jucrz <juliencruz78@gmail.com>
2021-06-01 11:44:21 +0200

    function connection

--- Create_BDD.py
+++ Create_BDD.py
@@ -1,7 +1,7 @@
 import pymysql
 
 def Create_table():
-    #database connection
+    # Database connection
     connection = pymysql.connect(host="localhost",user="root",passwd="",database="b2_proj_dev_log" )
     cursor = connection.cursor()
 
@@ -14,6 +14,44 @@ def Create_table():
     WIN INT(20) NOT NULL,
     LOOSE INT(20) NOT NULL)"""
 
-    # some other statements  with the help of cursor
     cursor.execute(JoueurTableSql)
+
+    # Close database connection
     connection.close()
+
+def Connection(log):
+    # Database connection
+    connection = pymysql.connect(host="localhost", user="root", passwd="", database="b2_proj_dev_log")
+    cursor = connection.cursor()
+
+    # Loop function connection
+    ConnectionUser = False
+    while not ConnectionUser:
+        print("Nom de compte :")
+        ndc = input()
+        cursor.execute("Select * from Joueur where Name = %s", ndc)
+        test = "0"
+        for row in cursor:
+            test = row
+        print(test)
+        print("Mot de passe :")
+        mdp = input()
+        cursor.execute("Select * from Joueur where Password = %s", mdp)
+        test2 = "1"
+        for row in cursor:
+            test2 = row
+        print(test2)
+        if test == test2:
+            ConnectionUser = True
+            log = test
+        else:
+            print("Nom de compte ou mot de passe incorrecte.")
+
+    # Close database connection
+    connection.close()
+
+    # Return information of the account
+    if ConnectionUser:
+        print("Tu es connecté !")
+        return log
+
